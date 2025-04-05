66ef4044da4e9ceb3302989a4d4b5e67ab3333f5
Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>
2025-04-05 09:44:27 +0700

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Meditation.Application.Commands;
+using Meditation.Application.Queries;
+using Meditation.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Meditation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController(ISender sender) : ControllerBase
+    {
+        [HttpPost("")] // (1)
+        public async Task<IActionResult> AddEmployeeAsync([FromBody] EmployeeEntity employee)
+        {
+            var result = await sender.Send(new AddEmployeeCommand(employee));
+            return Ok(result);
+        }
+        
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllEmployeeAsync()
+        {
+            var result = await sender.Send(new GetAllEmployeesQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{employeeId}")]
+        public async Task<IActionResult> GetAllEmployeeByIdAsync([FromRoute] Guid employeeId)
+        {
+            var result = await sender.Send(new GetEmployeeByIdQuery(employeeId));
+            return Ok(result);
+        }
+        
+        [HttpPut("{employeeId}")]
+        public async Task<IActionResult> UpdateEmployeeAsync([FromRoute] Guid employeeId, [FromBody] EmployeeEntity employee)
+        {
+            var result = await sender.Send(new UpdateEmployeeCommand(employeeId, employee));
+            return Ok(result);
+        }  
+        
+        [HttpDelete("{employeeId}")]
+        public async Task<IActionResult> DeleteEmployeeAsync([FromRoute] Guid employeeId)
+        {
+            var result = await sender.Send(new DeleteEmployeeCommand(employeeId));
+            return Ok(result);
+        }
+        
+
+    }
+}
---
+++ DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Meditation.Application;
+using Meditation.Infrastructure;
+
+namespace Meditation.API
+{
+    public static class DependencyInjection
+    {
+        // DI 
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
